a931982aeb9927a18b16dbebdde52677a70f7728
JT <j.j.teijema@uu.nl>
2024-04-08 10:54:09 +0200

    noqa all the things

--- asreviewcontrib/models/__init__.py
+++ asreviewcontrib/models/__init__.py
@@ -1,17 +1,17 @@
-from .multilingual import MultilingualSentenceTransformer
-from .wide_doc2vec import wide_doc2vec
-from .xgboost import XGBoost
-from .cnn import Power_CNN
-from .labse import LaBSE
-from .fasttext import FastTextFeatureExtractor
-from .onehot import OneHot
-from .spacy import SpacyEmbeddingExtractor
-from .word2vec import Word2VecModel
-from .dynamic_nn import DynamicNNClassifier
-from .adaboost import AdaBoost
-from .knn import KNN
-from .random import RandomClassifier
-from .ftsbert import FullTextSBERTModel
-from .relu_doc2vec import ReLUDoc2Vec
-from .softmax_doc2vec import SoftMaxDoc2Vec
-from .scaled_doc2vec import ScaledDoc2Vec
+from .multilingual import MultilingualSentenceTransformer  # noqa: F401
+from .wide_doc2vec import wide_doc2vec  # noqa: F401
+from .xgboost import XGBoost  # noqa: F401
+from .cnn import Power_CNN  # noqa: F401
+from .labse import LaBSE  # noqa: F401
+from .fasttext import FastTextFeatureExtractor  # noqa: F401
+from .onehot import OneHot  # noqa: F401
+from .spacy import SpacyEmbeddingExtractor  # noqa: F401
+from .word2vec import Word2VecModel  # noqa: F401
+from .dynamic_nn import DynamicNNClassifier  # noqa: F401
+from .adaboost import AdaBoost  # noqa: F401
+from .knn import KNN  # noqa: F401
+from .random import RandomClassifier  # noqa: F401
+from .ftsbert import FullTextSBERTModel  # noqa: F401
+from .relu_doc2vec import ReLUDoc2Vec  # noqa: F401
+from .softmax_doc2vec import SoftMaxDoc2Vec  # noqa: F401
+from .scaled_doc2vec import ScaledDoc2Vec  # noqa: F401
